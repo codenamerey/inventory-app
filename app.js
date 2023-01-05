ab79316d360df62fb2342e7dc9945302019da331
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-05 19:19:22 +0800

    Change router url from shop to store

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const shopRouter = require('./routes/shop');
+const storeRouter = require('./routes/store');
 
 var app = express();
 
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/shop', shopRouter);
+app.use('/store', storeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/store.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const itemController = require('../controllers/itemController');
+const sellerController = require('../controllers/sellerController');
+
+router.get('/', itemController.index);
+
+router.get('/category/:category', itemController.item_category_get);
+
+router.get('/item/:id', itemController.item_id_get);
+
+module.exports = router;
