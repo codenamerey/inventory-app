3008d5108c2087e84207f8b2b28526d7200dd627
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-05 14:57:32 +0800

    Change item schema datatype to Number and fix typo

--- models/item.js
+++ models/item.js
@@ -3,14 +3,15 @@ const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
     name: {type: String, required:true},
-    seller: {type: Object.Types.id, ref: "Seller", required: true},
-    price: {type:Float32Array, required:true},
+    seller: {type: Schema.Types.ObjectId, ref: 'Seller', required: true},
+    price: {type:Number, required:true},
     desc: {type:String, required:true},
-    available: {type:Int32Array, required:true}
+    available: {type:Number, required:true},
+    category: {type:String}
 })
 
 ItemSchema.virtual('url').get(function() {
     return `/store/item/${this._id}`;
 });
 
-module.exports = mongoose.Model('Item', ItemSchema);
+module.exports = mongoose.model('Item', ItemSchema);
