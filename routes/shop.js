8f13f2c5d452dd6d1177f9e79275ef62ae6d51d7
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-06 15:50:07 +0800

    Add style and finish project

--- routes/shop.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const itemController = require('../controllers/itemController');
-const sellerController = require('../controllers/sellerController');
-
-router.get('/', itemController.index);
-
-module.exports = router;
